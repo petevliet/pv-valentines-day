7c95dd4062d683e780f765657df2da9b1cfbb7e6
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-02-12 20:29:04 -0700

    remove turbolinks improve message controller begin table on rootpage

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,24 @@
 class MessagesController < ApplicationController
 
   def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to messages_path
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:author, :message, :anonymous, :image_pick, :title)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213005018) do
+ActiveRecord::Schema.define(version: 20150213031055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150213005018) do
     t.integer  "image_pick"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
